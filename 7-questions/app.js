2000969c3c5e758fd0f7f279688033a6c33d43f4
antonypauson <13antonysun@gmail.com>
2025-07-07 23:17:27 +0530

    question better approach

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,12 +1,22 @@
-//traversing the dom
 
-//select question-btn
-const buttons = document.querySelectorAll(".question-btn"); 
 
-buttons.forEach((button) => {
+// // selecting button and then moving up
+// const buttons = document.querySelectorAll(".question-btn"); 
+// buttons.forEach((button) => {
+//     button.addEventListener("click", function(e) {
+//         // console.log(e.currentTarget.parentElement); //nearest parent
+//         const question = e.currentTarget.parentElement.parentElement; //parent's parent
+//         question.classList.toggle("show-text"); 
+//     })
+// });
+
+// selecting question and then moving down
+// better approach
+const questions = document.querySelectorAll(".question"); 
+questions.forEach((question) => {
+    const button = question.querySelector(".question-btn"); 
     button.addEventListener("click", function(e) {
-        // console.log(e.currentTarget.parentElement); //nearest parent
-        const question = e.currentTarget.parentElement.parentElement; //parent's parent
+        // console.log(e.currentTarget.classList); 
         question.classList.toggle("show-text"); 
     })
-});
+})
