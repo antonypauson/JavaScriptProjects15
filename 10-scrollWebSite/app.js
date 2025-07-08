48605216588007efb048a5a5110f01e695523304
antonypauson <13antonysun@gmail.com>
2025-07-09 00:09:10 +0530

    navtoggle

---
+++ 10-scrollWebSite/app.js
@@ -0,0 +1,21 @@
+//date set up
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear(); 
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+    // linksContainer.classList.toggle("show-links"); 
+    const linksContainerHeight = linksContainer.getBoundingClientRect().height; 
+    const linksHeight = links.getBoundingClientRect().height; 
+    
+    if (linksContainerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0; 
+    }
+})
+
