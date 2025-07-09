7a58ecabc4b7cc0cb85f7a1eaf96df47aa71e9fe
antonypauson <13antonysun@gmail.com>
2025-07-09 12:47:09 +0530

    scroll website

--- 10-scrollWebSite/app.js
+++ 10-scrollWebSite/app.js
@@ -19,3 +19,53 @@ navToggle.addEventListener("click", () => {
     }
 })
 
+const navbar = document.getElementById("nav");
+const backButton = document.querySelector(".top-link");
+
+
+//pageYOffset : vertical scrolled pixels
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height; 
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav"); 
+        linksContainer.style.height = 0; 
+    } else {
+        navbar.classList.remove("fixed-nav"); 
+    }
+
+    if (scrollHeight > 500) {
+        backButton.classList.add('show-link');
+    } else {
+        backButton.classList.remove('show-link');
+    }
+});
+
+const allLinks = document.querySelectorAll(".scroll-link");
+
+allLinks.forEach((link) => {
+    link.addEventListener("click", event => {
+        //prevent default
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height; 
+        const fixedNav = navbar.classList.contains("fixed-nav");
+
+        
+        const position = element.offsetTop - navHeight;
+
+       
+
+        window.scrollTo({
+            left: 0, 
+            top: position
+        });
+        linksContainer.style.height = 0; 
+    })
+})
+
+
+
