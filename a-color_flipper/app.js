c138cb07a06ae0c30d370936f5e96ca71e817f10
antonypauson <13antonysun@gmail.com>
2025-07-23 01:35:20 +0530

    starting js

---
+++ a-color_flipper/app.js
@@ -0,0 +1,12 @@
+// console.log("HI")
+
+const colors = ["white", "salmon", "pink", "khaki"];
+
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", randomColor);
+
+function randomColor() {
+    console.log("clicked");
+}
