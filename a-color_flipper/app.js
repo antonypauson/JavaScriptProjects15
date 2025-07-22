e5bc2e374d65987a7e1082d065f87c53b74b6b00
antonypauson <13antonysun@gmail.com>
2025-07-23 01:58:12 +0530

    simple color flipper

--- a-color_flipper/app.js
+++ a-color_flipper/app.js
@@ -1,12 +1,23 @@
 // console.log("HI")
 
-const colors = ["white", "salmon", "pink", "khaki"];
+const colors = ["salmon", "pink", "khaki", "red", "yellow", "grey", "violet", "magenta", "brown", "navyblue"];
 
 
 const button = document.querySelector(".btn");
+const mainSection = document.querySelector(".main-section");
+
 
 button.addEventListener("click", randomColor);
 
 function randomColor() {
-    console.log("clicked");
+    const randColor = colors[getRandomNumber()];
+
+    // const computedStyles = window.getComputedStyle(mainSection);
+    // console.log(computedStyles.backgroundColor);
+    
+    mainSection.style.backgroundColor = randColor; 
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
 }
